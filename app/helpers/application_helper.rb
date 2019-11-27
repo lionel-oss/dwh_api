11905708c3ffdf875be018ce930bce093c1e758b
Odmen <odmen@ip-192-168-0-85.ec2.internal>
2019-11-27 13:17:01 +0200

    handle empty array of hashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ require 'csv'
 
 module ApplicationHelper
   def to_csv(array_of_hashes)
+    return [] unless array_of_hashes.first
+
     CSV.generate do |csv|
       csv << array_of_hashes.first.keys
       array_of_hashes.map { |hash| csv << hash.values }
